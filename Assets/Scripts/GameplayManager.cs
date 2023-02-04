57a2f9cf51225de4fbfcd9ec415054824f21bd99
Connor Bews <connorbews@MacBook-Pro-3.local>
2023-02-04 02:39:35 -0800

    Gameplay is finished

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class GameplayManager : MonoBehaviour
 {
-    public int spore1 = 0;
-    public int spore2 = 0;
-    public int spore3 = 0;
+    private int spore1 = 0;
+    private int spore2 = 0;
+    private int spore3 = 0;
 
     public gameState state;
     // Start is called before the first frame update
@@ -18,12 +18,43 @@ public class GameplayManager : MonoBehaviour
         spore3 = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public int GetSpore1()
     {
-        
+        return spore1;
     }
 
+    public int GetSpore2()
+    {
+        return spore2;
+    }
+
+    public int GetSpore3()
+    {
+        return spore3;
+    }
+
+    public void SetSpore1(int addition)
+    {
+        spore1 = addition;
+    }
+
+    public void SetSpore2(int addition)
+    {
+        spore2 = addition;
+    }
+
+    public void SetSpore3(int addition)
+    {
+        spore3 = addition;
+    }
+
+    public void IncreaseLevel()
+    {
+        if (state != gameState.Level3)
+        {
+            state += 1;
+        }
+    }
 
     public enum gameState
     {
